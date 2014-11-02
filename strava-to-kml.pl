a82cec7c55f9d39562061983456b128bf9ffe4fa
Lee Aylward <lee@laylward.com>
2014-11-01 22:06:59 -0400

    get basic google map rendering polyline

--- strava-to-kml.pl
+++ strava-to-kml.pl
@@ -9,11 +9,14 @@ use AnyEvent::HTTPD;
 use JSON::XS;
 use URI;
 use URI::QueryParam;
+use Text::Xslate;
 
 my ($id, $secret) = do {
   open(my $fh, "<", "secret") or die "missing secret";
   split " ", <$fh>;
 };
+
+my $activity_id = $ARGV[0] or die "need activity id";
 
 my $base = "https://www.strava.com";
 my $auth = URI->new("$base/oauth/authorize");
@@ -31,16 +34,30 @@ my $httpd = AnyEvent::HTTPD->new(port => 8001);
 
 $httpd->reg_cb("/" => sub {
   my ($httpd, $req) = @_;
-  $req->respond([200, "OK", {"Content-Type" => "text/plain"}, "success"]);
-  $cv->send($req->parm("code"));
+  $cv->send($req);
 });
 
-my $code = $cv->recv;
-
+my $req = $cv->recv;
+my $code = $req->parm("code");
 $cv = AE::cv;
+
 http_post "$base/oauth/token",
   "client_id=$id&client_secret=$secret&code=$code",
   sub { $cv->send(decode_json $_[0]) };
 
 my $user = $cv->recv;
+$cv = AE::cv;
+http_get "$base/api/v3/activities/$activity_id",
+  headers => {Authorization => "Bearer $user->{access_token}"},
+  sub { $cv->send(decode_json $_[0]) };
 
+my $activity = $cv->recv;
+my $xslate = Text::Xslate->new(function => {
+  encode_json => \&JSON::XS::encode_json
+});
+$req->respond([
+  200,
+  "OK",
+  {"Content-Type" => "text/html"},
+  $xslate->render("gmap.tx", {user => $user, activity => $activity})
+]);
